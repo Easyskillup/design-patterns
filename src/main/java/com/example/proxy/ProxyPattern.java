26da52ac1088044957884fcaef7c4b19325748f4
ashish-panicker <ashish.s.panicker@proton.me>
2025-01-22 10:29:10 +0530

    Proxy pattern

---
+++ src/main/java/com/example/proxy/ProxyPattern.java
@@ -0,0 +1,65 @@
+package com.example.proxy;
+
+/**
+ * Subject interface
+ */
+interface VideoService {
+  void playVideo(String videoUrl);
+}
+
+/*
+ * Subject implementation
+ */
+class Video4KServiceImpl implements VideoService {
+  @Override
+  public void playVideo(String videoUrl) {
+    System.out.println("Playing 4k Video");
+  }
+}
+
+class VideoHDServiceImpl implements VideoService {
+  @Override
+  public void playVideo(String videoUrl) {
+    System.out.println("Playing HD Video");
+  }
+}
+
+/**
+ * Create the proxy
+ */
+class VideoServiceProxy implements VideoService {
+
+  private VideoService videoService;
+  private final String subscription;
+
+  public VideoServiceProxy(String subscription) {
+    this.subscription = subscription;
+  }
+
+  @Override
+  public void playVideo(String videoUrl) {
+
+    switch (subscription) {
+      case "premium":
+        videoService = new Video4KServiceImpl();
+        break;
+      case "regular":
+        videoService = new VideoHDServiceImpl();
+        break;
+      default:
+        break;
+    }
+    videoService.playVideo(videoUrl);
+  }
+}
+
+public class ProxyPattern {
+  public static void main(String[] args) {
+
+    VideoService premiumProxy = new VideoServiceProxy("premium");
+    VideoService regularProxy = new VideoServiceProxy("regular");
+
+    premiumProxy.playVideo("movie");
+    regularProxy.playVideo("movie");
+  }
+}
